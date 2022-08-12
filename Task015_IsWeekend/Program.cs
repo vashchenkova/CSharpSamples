4970697a46abf7ce4fb96c886b4dd492908b5bef
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-12 18:11:39 +0300

    Add files via upload

---
+++ Task015_IsWeekend/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Print number (1-7)");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b == 6 | b == 7)
+{
+    Console.WriteLine("weekend");
+}
+else
+{
+    Console.WriteLine("not a weekend");
+}
+
---
+++ Task016_IsSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine("b - квадрат a");
+    return;
+}
+if (b * b == a)
+{
+    Console.WriteLine("a квадрат b");
+}
+else
+{
+    Console.WriteLine("не является");
+}
+
+
+
