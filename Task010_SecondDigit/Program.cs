0b1e7d55ade4d11a0ed9d28cb86bd4bc6eba89fb
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-12 17:54:17 +0300

    Add files via upload

--- Task010_SecondDigit/Program.cs
+++ Task010_SecondDigit/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Number = 0;
+Console.WriteLine("Введите трёхзначное число: " );
+Number = Convert.ToInt32(Console.ReadLine());;
+if (Number >= 100 && Number <= 999)
+{
+    int DecreaseByTen = Number / 10;
+    int LastDigit = DecreaseByTen % 10;
+    Console.WriteLine("Вторая цифра числа =" + LastDigit);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число!");
+}
