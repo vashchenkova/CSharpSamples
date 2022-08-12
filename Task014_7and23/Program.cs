649e23921641e613b8d62c522eabb0bb9e5f0fce
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-12 17:57:10 +0300

    Add files via upload

---
+++ Task014_7and23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool ismultiplicity(int a)
+{
+    return (a % 7 == 0 & a % 23 ==0);
+}
+if (ismultiplicity(n)) {
+    Console.WriteLine("Делится!");
+}
+
+else {
+    Console.WriteLine("Не делится!");
+}
