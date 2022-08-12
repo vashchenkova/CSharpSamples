a8a3980034e70eb6758f44d3e7c7636aedea65d0
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-12 18:19:16 +0300

    Add files via upload

---
+++ Task030_8Random/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8]; 
+int len = array.Length;
+int index = 0;
+while(index < len)
+{
+    array[index] = new Random().Next(0,2);
+    index++;
+}
+for (int i=0; i<len; i++)
+{
+Console.Write(array[i]);
+}
