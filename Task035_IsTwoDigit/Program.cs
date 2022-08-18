da93a8b7aa81ca4ec8c0e3d4cdd65699c1ea6431
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-18 10:42:08 +0300

    Add files via upload

---
+++ Task035_IsTwoDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, World!");
+
+int[] array = new int[123];
+int len = array.Length;
+int count = 0;
+
+for(int i = 0 ; i < len; i++)
+{
+    array[i] = new Random().Next(0,500);
+    Console.Write($"{array[i]} ");
+}
+
+for(int i = 0 ; i < len; i++)
+{
+    if (array[i] >= 10 & array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"чисел двухзначных чисел = {count}");
---
+++ Task037_PairMul/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = {6, 7, 6, 3};
+int len = array.Length;
+int index = 0;
+
+Console.Write("Массив = ");
+while (index < len)
+{
+//    array[index] = new Random().Next(-10, 10);
+    Console.Write(array[index]); 
+    Console.Write(" ");
+    index++;
+}
+Console.WriteLine();
+index = 0;
+if (len % 2 == 0) {
+    while (index < len / 2){
+        Console.Write(array[index] * array[len - index - 1] + " ");
+        index += 1;
+    }
+}
+else {
+    while (index < len / 2){
+        Console.Write(array[index] * array[len - index - 1] + " ");
+        index += 1;
+    }
+    Console.Write(array[index]);
+}
